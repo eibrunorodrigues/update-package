1a889d009644b008fbe87a0eebca231d08dcc710
Bruno Rodrigues <eibrunorodrigues@gmail.com>
2020-12-09 10:36:59 -0300

    Refactoring and cleaning a little bit

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/eibrunorodrigues/update-packages/utils"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -16,6 +17,10 @@ import (
 	"time"
 )
 
+const (
+	PythonPKGVersionSetter = "=="
+)
+
 type PackageModel struct {
 	Package        string
 	CurrentVersion string
@@ -25,12 +30,11 @@ type PackageModel struct {
 type Arguments struct {
 	Path     string
 	FileName string
-	Branchs  []string
+	Branches []string
 	Language string
 }
 
 func initApplication() {
-	fatalErrors := make(chan string)
 	var arguments Arguments
 	registerArgs(&arguments)
 	validateArgs(&arguments)
@@ -41,37 +45,9 @@ func initApplication() {
 
 	var packages []PackageModel
 
-	getPackagesRequirements(&fatalErrors, arguments.Path, &packages)
+	getPackagesRequirements(arguments.Path, &packages)
 	askForEachPackage(&packages)
 	updateRequirements(arguments.Path, packages)
-
-	for errors := range fatalErrors {
-		log.Fatalln(errors)
-	}
-}
-
-func ask() bool {
-	okayResponses := []string{"y", "Y", "yes", "Yes", "YES"}
-	notOkayResponses := []string{"n", "N", "no", "No", "NO"}
-	allAnswers := addToList(okayResponses, notOkayResponses)
-	loop := 0
-	var responseStr string
-
-	for !containsString(allAnswers, responseStr) {
-		if loop > 0 {
-			fmt.Println("Wrong answer... Allowed responses are: y/N")
-		}
-
-		fmt.Print("\nType your answer... y/N : ")
-		_, err := fmt.Scanln(&responseStr)
-
-		if err != nil {
-			log.Panic(err.Error())
-		}
-		loop += 1
-	}
-
-	return containsString(okayResponses, responseStr)
 }
 
 func askForEachPackage(packages *[]PackageModel) {
@@ -87,10 +63,10 @@ func askForEachPackage(packages *[]PackageModel) {
 
 		if packageItem.CurrentVersion == "" {
 			fmt.Print("\nWould you like to set a static version on this package? ")
-			response = ask()
+			response = utils.Ask()
 		} else {
 			fmt.Print("\nWould you like to update it? ")
-			response = ask()
+			response = utils.Ask()
 		}
 
 		if response {
@@ -103,37 +79,6 @@ func askForEachPackage(packages *[]PackageModel) {
 
 		(*packages)[index].NewVersion = packageItem.CurrentVersion
 	}
-}
-
-func addToList(listA []string, listB []string) []string {
-	sizeOfArray := len(listA) + len(listB)
-	finalSlice := make([]string, sizeOfArray)
-	counter := 0
-
-	for index, _ := range listB {
-		finalSlice[counter] = listB[index]
-		counter += 1
-	}
-
-	for index, _ := range listA {
-		finalSlice[counter] = listA[index]
-		counter += 1
-	}
-
-	return finalSlice
-}
-
-func containsString(slice []string, element string) bool {
-	return !(posString(slice, element) == -1)
-}
-
-func posString(slice []string, element string) int {
-	for index, elem := range slice {
-		if elem == element {
-			return index
-		}
-	}
-	return -1
 }
 
 func updateRequirements(directory string, packages []PackageModel) {
@@ -156,12 +101,12 @@ func updateRequirements(directory string, packages []PackageModel) {
 				continue
 			}
 
-			if strings.Contains(data, "==") {
-				version := strings.Split(data, "==")
+			if strings.Contains(data, PythonPKGVersionSetter) {
+				version := strings.Split(data, PythonPKGVersionSetter)
 				newVersion := getVersion(version[0], packages)
 
 				if newVersion != "" {
-					finalFile += version[0] + "==" + newVersion + "\n"
+					finalFile += version[0] + PythonPKGVersionSetter + newVersion + "\n"
 					continue
 				}
 				finalFile += version[0] + "\n"
@@ -174,7 +119,7 @@ func updateRequirements(directory string, packages []PackageModel) {
 				continue
 			}
 
-			finalFile += data + "==" + newVersion + "\n"
+			finalFile += data + PythonPKGVersionSetter + newVersion + "\n"
 		}
 		updateRepo(versionFile[1], finalFile)
 	}
@@ -198,43 +143,35 @@ func updateRepo(privateRequirementsPath string, finalFile string) {
 	for _, branch := range [2]string{"develop", "release"} {
 
 		fmt.Print("\nDo you want to update the branch: " + branch + " of " + currentPackage + "? ")
-		doUpdate := ask()
-
-		if doUpdate {
-			err = w.Reset(&git.ResetOptions{Mode: git.ResetMode(git.HardReset)})
-			if err != nil {
+		if utils.Ask() {
+			if err = w.Reset(&git.ResetOptions{Mode: git.ResetMode(git.HardReset)}); err != nil {
 				log.Panic(err.Error())
 			}
 
-			err = w.Checkout(&git.CheckoutOptions{Branch: plumbing.NewBranchReferenceName(branch)})
-			if err != nil {
+			if err = w.Checkout(&git.CheckoutOptions{Branch: plumbing.NewBranchReferenceName(branch)}); err != nil {
 				log.Panic(err.Error())
 			}
 
-			err = w.Pull(&git.PullOptions{RemoteName: "origin"})
-			if err != nil && !strings.Contains(err.Error(), "up-to-date") {
+			if err = w.Pull(&git.PullOptions{RemoteName: "origin"}); err != nil && !strings.Contains(err.Error(), "up-to-date") {
 				log.Panic(err.Error())
 			}
 
-			err := ioutil.WriteFile(privateRequirementsPath, []byte(finalFile), 0755)
-			if err != nil {
+			if err := ioutil.WriteFile(privateRequirementsPath, []byte(finalFile), 0755); err != nil {
 				log.Panic("Error saving file " + err.Error())
 			}
 
-			_, err = w.Commit("Updating Packages via script...", &git.CommitOptions{Author: &object.Signature{Name: "Bruno Rodrigues", Email: "bruno.dasilva@ambevtech.com.br", When: time.Now()}})
-			if err != nil {
+			if _, err = w.Commit("Updating Packages via script...", &git.CommitOptions{Author: &object.Signature{Name: "Bruno Rodrigues", Email: "bruno.dasilva@ambevtech.com.br", When: time.Now()}}); err != nil {
 				log.Panic(err.Error())
 			}
 
-			err = r.Push(&git.PushOptions{RemoteName: branch, RefSpecs: []config.RefSpec{"refs/remotes/origin/*"}})
-			if err != nil {
+			if err = r.Push(&git.PushOptions{RemoteName: branch, RefSpecs: []config.RefSpec{"refs/remotes/origin/*"}}); err != nil {
 				log.Panic(err.Error())
 			}
 		}
 	}
 }
 
-func getPackagesRequirements(fatalErrors *chan string, directory string, packages *[]PackageModel) {
+func getPackagesRequirements(directory string, packages *[]PackageModel) {
 	for versionFile := range findAllFilesToUpdate(directory) {
 		fileData := strings.Split(string(versionFile[0]), "\n")
 
@@ -243,8 +180,8 @@ func getPackagesRequirements(fatalErrors *chan string, directory string, package
 				continue
 			}
 
-			if strings.Contains(data, "==") {
-				version := strings.Split(data, "==")
+			if strings.Contains(data, PythonPKGVersionSetter) {
+				version := strings.Split(data, PythonPKGVersionSetter)
 				if !contains(*packages, version[0]) {
 					*packages = append(*packages, PackageModel{Package: version[0], CurrentVersion: version[1]})
 				}
@@ -302,7 +239,7 @@ func validateArgs(args *Arguments) {
 		log.Fatalln("The argument " + args.Path + " is not a valid folder")
 	}
 
-	for _, branch := range args.Branchs {
+	for _, branch := range args.Branches {
 		if branch != "develop" && branch != "release" && branch != "master" {
 			log.Fatalln("Branch " + branch + " not allowed ")
 		}
@@ -311,18 +248,18 @@ func validateArgs(args *Arguments) {
 }
 
 func registerArgs(args *Arguments) {
-	var branchs string
+	var branches string
 	flag.StringVar(&args.FileName, "file", "private-requirements.txt", "Name of the File to change")
 	flag.StringVar(&args.Path, "path", "~/", "Path to the Projects")
-	flag.StringVar(&branchs, "branch", "develop,release", "Branchs to update splitted with commas")
+	flag.StringVar(&branches, "branch", "develop,release", "Branches to update splitted with commas")
 	flag.StringVar(&args.Language, "lang", "python", "The programming language you would like to update the packages")
 	flag.Parse()
 
-	args.Branchs = strings.Split(branchs, ",")
+	args.Branches = strings.Split(branches, ",")
 }
 
 func main() {
-	fmt.Println("Starting to update canaa-packages")
+	fmt.Println("Starting to update packages")
 	initApplication()
 	fmt.Println("Ending Process")
 }
---
+++ utils/input.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+)
+
+func Ask() bool {
+	okayResponses := []string{"y", "Y", "yes", "Yes", "YES"}
+	notOkayResponses := []string{"n", "N", "no", "No", "NO"}
+	allAnswers := addToList(okayResponses, notOkayResponses)
+	loop := 0
+	var responseStr string
+
+	for !containsString(allAnswers, responseStr) {
+		if loop > 0 {
+			fmt.Println("Wrong answer... Allowed responses are: y/N")
+		}
+
+		fmt.Print("\nType your answer... y/N : ")
+		_, err := fmt.Scanln(&responseStr)
+
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		loop += 1
+	}
+
+	return containsString(okayResponses, responseStr)
+}
+
+func containsString(slice []string, element string) bool {
+	return !(posString(slice, element) == -1)
+}
+
+func posString(slice []string, element string) int {
+	for index, elem := range slice {
+		if elem == element {
+			return index
+		}
+	}
+	return -1
+}
+
+func addToList(listA []string, listB []string) []string {
+	sizeOfArray := len(listA) + len(listB)
+	finalSlice := make([]string, sizeOfArray)
+	counter := 0
+
+	for index, _ := range listB {
+		finalSlice[counter] = listB[index]
+		counter += 1
+	}
+
+	for index, _ := range listA {
+		finalSlice[counter] = listA[index]
+		counter += 1
+	}
+
+	return finalSlice
+}
